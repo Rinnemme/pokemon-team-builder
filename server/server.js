52e978f18cbd8da2281dc4501bca9c8d6238a00b
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-04-25 17:33:00 -0400

    update server.js to include new routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const pokemonRoutes = require('./routes/pokemon')
+const teamRoutes = require('./routes/teams')
+const typeRoutes = require('./routes/types')
 const app = express()
 
 //middleware
@@ -13,10 +15,12 @@ app.use((req,res,next) => {
 })
 
 //routes
+app.use('/teams', teamRoutes)
 app.use('/pokemon', pokemonRoutes)
+app.use('/types', typeRoutes)
 
 //connect to Mongoose
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {dbName: 'Pokemon-Team-Builder'})
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('connected & listening on port', process.env.PORT)

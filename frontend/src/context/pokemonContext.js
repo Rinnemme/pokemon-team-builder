f3089e4fd22717c2c992cdf5970eb48ccb66cf8a
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-04-27 22:08:53 -0400

    remove commented out code from PokemonContext and have the default value of myTeam be an object rather than null

--- frontend/src/context/pokemonContext.js
+++ frontend/src/context/pokemonContext.js
@@ -4,17 +4,12 @@ export const PokemonContext = createContext()
 
 export const pokeReducer = (state, action) => {
     switch (action.type) {
-        // case 'SET_TEAMS':
-        //     return {teams: action.payload}
-        // case 'SET_POKEMON':
-        //     return {pokemon: action.payload}
-        // case 'SET_TYPES':
-        //     return {types: action.payload}
         case 'SET_ALL':
             return {
                 pokemon: action.payload[0],
                 teams: action.payload[1],
-                types: action.payload[2]
+                types: action.payload[2],
+                myTeam: action.payload[3]
             }
         case 'ADD_TO_TEAM':
             return {myTeam: [...state.myTeam, action.payload]}
@@ -28,7 +23,7 @@ export const pokeReducer = (state, action) => {
 }
 
 export const PokemonContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(pokeReducer, {pokemon: null, teams: null, types: null, myTeam: null})
+    const [state, dispatch] = useReducer(pokeReducer, {pokemon: null, teams: null, types: null, myTeam: {name: null, members: []}})
     return (
         <PokemonContext.Provider value = {{...state, dispatch}}>
             {children}

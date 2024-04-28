95984fe457f20d3eef87391cb1e5d08cfb0efba3
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-04-27 22:09:25 -0400

    clean up navbar component to only have a header with a link inside; the other divs were superfluous

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,11 +3,9 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return (
         <header>
-            <div className="container">
                 <Link to="/">
                     <h1>Pokemon Team Builder</h1>
                 </Link>
-            </div>
         </header>
     )
 }

46dc9941d30cdfd9bfdd79395ca1d53d010bc00e
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-04-29 19:15:10 -0400

    add depoyed-url/my-team to CORS allowed origins

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
 })
-app.use(cors({origin: ["https://pokemon-team-builder-usjx.vercel.app", "http://localhost:3000"]}))
+app.use(cors({origin: ["https://pokemon-team-builder-usjx.vercel.app", "https://pokemon-team-builder-usjx.vercel.app/my-team", "http://localhost:3000"]}))
 
 // return a basic home page
 app.get('/', (req, res) => {
